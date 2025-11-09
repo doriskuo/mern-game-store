6c0cee16936f1ab8633d578cda946e538f6a6275
doriskuo <doriskuo0105@gmail.com>
2025-11-09 22:36:56 +0800

    fix contents for deploy

--- client/react-router.config.ts
+++ client/react-router.config.ts
@@ -1,7 +1,5 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
-  // Config options...
-  // Server-side render by default, to enable SPA mode set this to `false`
-  ssr: true,
+  ssr: false, // 暫時關閉 SSR，快速驗證 500 是否來自於伺服器端渲染
 } satisfies Config;
